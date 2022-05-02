2f39566a5ddfef185e72f36b2f6a8c2a14f7c055
elfsvet <stepanmatysik@gmail.com>
2022-05-01 22:25:52 -0400

    added generate Project function and used map and filter method

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -130,15 +130,18 @@ const promptProject = portfolioData => {
     });
 };
 
+// don't forget to switch it back
+// const pageHTML = generatePage(mockData);
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    // console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+    
+      console.log('Portfolio complete! Check out index.html to see the output!');
+    });
+    
   });
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
